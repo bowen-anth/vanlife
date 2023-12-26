ad16e2ca1e40ce8b988ee1e706d1a797666554f6
bowen-anth <bowen.anth@gmail.com>
2023-12-26 14:09:20 -0700

    home page done

--- components/Home.jsx
+++ components/Home.jsx
@@ -4,9 +4,13 @@ const Home = () => {
     return (
         <>
             <main className="home-main">
-                <p className="home-title-p">You got the travel plans, we got the travel vans.</p>
-                <p className="home-sub-title-p">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-                <button className="home-button">Find your van</button>
+                <div className="home-main-content-container">
+                    <div className="home-container-p">
+                    <p className="home-title-p">You got the travel plans, we got the travel vans.</p>
+                    <p className="home-sub-title-p">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+                    </div>
+                    <button className="home-button">Find your van</button>
+                </div>
                 <footer className="home-footer">
                     <p>Ⓒ 2022 #VANLIFE</p>
                 </footer>
