48634aff08c6779f430996cf4e9fa28602bea87a
bowen-anth <bowen.anth@gmail.com>
2023-12-27 11:13:30 -0700

    added van id page

--- components/VanDetail.jsx
+++ components/VanDetail.jsx
@@ -1,7 +1,39 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 const VanDetail = () => {
-    return <h1>Van detail page goes here</h1>
+    const params = useParams()
+    const [van, setVan] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch(`/api/vans/${params.id}`)
+            .then(res => res.json())
+            .then(data => setVan(data.vans))
+    }, [params.id])
+
+
+    return (
+        <>
+            <div className="van-detail-container">
+                {van ? (
+                    <div className="van-detail">
+                        <img src={van.imageUrl} 
+                        alt={van.title}
+                        width="400px"
+                        height="auto"
+                        />
+                        <div className="van-info-container">
+                            <button className={`van-type ${van.type} selected`}>{van.type}</button>
+                            <h2>{van.name}</h2>
+                            <p className="van-price"><span>${van.price}</span>/day</p>
+                            <p className="van-description-p">{van.description}</p>
+                            <button className="van-link-button">Rent this van</button>
+                        </div>
+                    </div>
+                ) : <h2>Loading...</h2>}
+             </div>
+        </>
+    )
 }
 
 export default VanDetail
