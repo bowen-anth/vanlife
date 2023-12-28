1f82e874729a5ae6b26e141975ff4c722f4c6638
bowen-anth <bowen.anth@gmail.com>
2023-12-28 15:37:33 -0700

    added host vans detail page and route

--- components/pages/Host/HostVanDetail.jsx
+++ components/pages/Host/HostVanDetail.jsx
@@ -1,9 +1,47 @@
 import React from "react"
+import { useParams, NavLink } from "react-router-dom"
 
-const HostVanDetail = () => {
+const HostVanDetail = (props) => {
+
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+        .then(res => res.json())
+        .then(data => setCurrentVan(data.vans))
+    }, [])
+// console.log('currentVan', currentVan)
+// console.log('name', currentVan.name)
+    // const hostVanDetailCard = currentVan.vans.map((van) => {
+    //     return (
+    //         <p>{van.title}</p>
+    //     )
+    // })
+
+    if (!currentVan) {
+        return <h1>Loading...</h1>
+    }
+
     return (
         <>
-            <h1>Host Van Detail</h1>
+            <div className="host-van-detail-container">
+                <div className="host-van-container">
+                <img src={currentVan.imageUrl} width={150} />
+                    <div className="host-van-info-container">
+                        <button>{currentVan.type}</button>
+                        <p className="host-van-name-p">{currentVan.name}</p>
+                        <p className="host-van-price-p">{currentVan.price}<span className="detail-day-span">/day</span></p>
+                    </div>
+                </div>
+                
+                <div className="host-van-detail-link-container">
+                    <NavLink to="">Details</NavLink>
+                    <NavLink to="">Pricing</NavLink>
+                    <NavLink to="">Photos</NavLink>
+                </div>
+
+            </div>
         </>
     )
 }
--- components/pages/Host/HostVans.jsx
+++ components/pages/Host/HostVans.jsx
@@ -1,9 +1,51 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 
 const HostVans = () => {
+// console.log('props', props)
+
+    const [hostVanData, setHostVanData] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("/api/host/vans")
+        .then(res => res.json())
+        .then(data => setHostVanData(data.vans))
+    }, 
+    [])
+ 
+
+    const hostVanCard = hostVanData.map(van => {
+        return (
+            <NavLink to={`/host/vans/${van.id}`}>
+                <div className="host-vans-van-container" key={van.id}>
+                <img src={van.imageUrl}
+                    width="400px"
+                    height="auto"
+                    alt={van.name}
+                />
+                <div className="host-vans-van-info-container">
+                    <p className="host-van-name-p">{van.name}</p>
+                    <p className="host-van-price-p">${van.price}/day</p>
+                </div>
+            </div>
+           </NavLink>
+        )
+    })
+
+    
+
     return (
         <>
-            <h1>Host Vans</h1>
+            <h1>Your listed Vans</h1>
+            {
+                    hostVanData.length > 0 ? (
+                        <section>
+                            {hostVanCard}
+                        </section>
+                    ) : (
+                            <h2>Loading...</h2>
+                        )
+                }
         </>
     )
 }
