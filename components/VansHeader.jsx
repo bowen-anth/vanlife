7ce6b0b1cac97f084e6725cd340152ddce7f4581
bowen-anth <bowen.anth@gmail.com>
2023-12-29 00:54:41 -0700

    styled btns to highlight when filter selected on Vans page

--- components/VansHeader.jsx
+++ components/VansHeader.jsx
@@ -4,7 +4,8 @@ import { useSearchParams } from "react-router-dom"
 const VansHeader = () => {
 
     const [searchParams, setSearchParams] = useSearchParams()
-
+    const typeFilter = searchParams.get("type")
+    console.log(typeFilter)
     return (
         <>
             <div className="vansHeader-container">
@@ -12,19 +13,19 @@ const VansHeader = () => {
                     <h1 className="vansHeader-h1">Explore our van options</h1>
                     <div className="vansHeader-button-container">
                         <button 
-                        className="vansHeader-button"
+                        className={typeFilter === "simple" ? "vansHeader-button simple selected" : "vansHeader-button"}
                         onClick={() => setSearchParams({type: "simple"})}
                         >
                             Simple
                         </button>
                         <button 
-                        className="vansHeader-button luxury"
+                        className={typeFilter === "luxury" ? "vansHeader-button luxury selected" : "vansHeader-button"}
                         onClick={() => setSearchParams({type: "luxury"})}
                         >
                             Luxury
                         </button>
                         <button 
-                        className="vansHeader-button rugged"
+                        className={typeFilter === "rugged" ? "vansHeader-button rugged selected" : "vansHeader-button"}
                         onClick={() => setSearchParams({type: "rugged"})}                    
                         >
                             Rugged
